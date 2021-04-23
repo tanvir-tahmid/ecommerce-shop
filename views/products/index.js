f38843601fb37f5dffa8f00c211fb862f4fe71cd
Pavilion <tanv.mhn@gmail.com>
2021-04-23 15:51:13 +0600

    first commit

---
+++ views/products/index.js
@@ -0,0 +1,58 @@
+const layout = require('../layout');
+
+module.exports = ({ products }) => {
+  const renderedProducts = products
+    .map(product => {
+      return `
+        <div class="column is-one-quarter">
+          <div class="card product-card">
+            <figure>
+              <img src="data:image/png;base64, ${product.image}"/>
+            </figure>
+            <div class="card-content">
+              <h3 class="subtitle">${product.title}</h3>
+              <h5>$${product.price}</h5>
+            </div>
+            <footer class="card-footer">
+              <form action="/cart/products" method="POST">
+                <input hidden value="${product.id}" name="productId" />
+                <button class="button has-icon is-inverted">
+                  <i class="fa fa-shopping-cart"></i> Add to cart
+                </button>
+              </form>
+            </footer>
+          </div>
+        </div>
+      `;
+    })
+    .join('\n');
+
+  return layout({
+    content: `
+      <section class="banner">
+        <div class="container">
+          <div class="columns is-centered">
+            <img src="https://images.unsplash.com/photo-1593238404535-cda7ae2fe50b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1051&q=80" />
+          </div>
+        </div>
+      </section>
+      
+      <section>
+        <div class="container">
+          <div class="columns">
+            <div class="column "></div>
+            <div class="column is-four-fifths">
+              <div>
+                <h2 class="title text-center">Featured Items</h2>
+                <div class="columns products">
+                  ${renderedProducts}  
+                </div>
+              </div>
+            </div>
+            <div class="column "></div>
+          </div>
+        </div>
+      </section>
+    `
+  });
+};
